320077c0146482683344ee69549a1c4b7f2f0396
Konds Sonawane <kondya@Kondirams-MacBook-Air.local>
2017-12-07 21:09:13 +0530

    textview

--- TextViewDemo/ViewController.m
+++ TextViewDemo/ViewController.m
@@ -8,22 +8,55 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextViewDelegate>
+{
+    UIImageView *imgView;
+}
+@property (weak, nonatomic) IBOutlet UITextView *textview;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.textview.delegate = self;
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)addImage:(id)sender {
+    
+    self.textview.text = @"test text";
+    
+   imgView = [[UIImageView alloc] init];
+    
+    imgView.image = [UIImage imageNamed:@"smily.jpg"];
+    imgView.layer.cornerRadius = 10;
+   
+    
+}
+-(void)textViewDidChange:(UITextView *)textView
+{
+    CGPoint cursorPosition = [self.textview caretRectForPosition:self.textview.selectedTextRange.start].origin;
+    [imgView setFrame:CGRectMake(0, cursorPosition.y+50, self.textview.frame.size.width, 300)];
+     [self.textview addSubview:imgView];
+    
+    CGFloat fixedWidth = textView.frame.size.width;
+    CGSize newSize = [textView sizeThatFits:CGSizeMake(fixedWidth, MAXFLOAT)];
+    CGRect newFrame = textView.frame;
+    newFrame.size = CGSizeMake(fmaxf(newSize.width, fixedWidth), newSize.height+300);
+    [textView setContentSize:CGSizeMake(newFrame.size.width, newFrame.size.height)];
+    
+    
+    
+}
 
 @end
